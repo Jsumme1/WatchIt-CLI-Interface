07569d593eebc3dcd895915775fd4f01938e88d5
Jsumme1 <88805050+Jsumme1@users.noreply.github.com>
2022-07-04 15:16:34 -0500

    builing nodmon

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
 #!/usr/bin/env node
 
-console.log ("I was exectued");
+const debounce = require("lodash.debounce");
+const chokidar = require("chokidar");
+const program = require("caporal");
+const fs = require('fs');
+const {spawn} = require('child_process');
+
+
+program .version("0.0.1")
+.argument('[filename]', 'Name of a file to execute')
+.action(async ( { filename }) => {
+    const name = filename || 'index.js';
+    
+    try{
+    await fs.promises.access(name);
+    } catch (err) {
+        throw new Error(`Could not find the file ${name}` );
+
+    }
+  // waits for all files to be "added" by chokidar 100ms
+  const start = debounce(() => {
+    spawn('node', [name], {stdio: 'inherit'});
+  }, 100);
+
+  // detects any file changes added (also initial opening), changed, deleted
+  chokidar
+    .watch(".")
+    .on("add", start)
+    .on("change", start)
+    .on("unlink", start);
+});
+
+program.parse(process.argv);
+
+
